a7883d6b33bcc0e2e8206a8eac47e6be674e36c8
Giantcard5 <giantcard5.dev@outlook.com>
2025-05-04 21:36:13 -0300

    feat: create vercel files

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -27,10 +27,9 @@ import {
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
 
+app.use(express.json());
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/hltv', hltvRouter);
 app.use('/api/users', userRouter);
@@ -44,6 +43,4 @@ app.get('/', (req, res) => {
     res.json({ message: 'FURIA FAN API is running' });
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+export default app;
